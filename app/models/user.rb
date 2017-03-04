fe6dbc08feccc34febfa35f27f7be705fa2fe961
paulachenhua <chenhua1718@gmail.com>
2017-03-04 11:02:14 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ApplicationRecord
   has_many :group_relationships
   has_many :participated_groups, :through => :group_relationships, :source => :group
-end# Include default devise modules. Others available are:
+# Include default devise modules. Others available are
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :groups
   has_many :posts
+
+
+def is_member_of?(group)
+    participated_groups.include?(group)
+
+  end
+
 end
